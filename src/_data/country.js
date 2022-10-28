290dc67a4f92f51d3a218ee079aed79c19be99d6
pppog <loke.oberg@elev.ga.ntig.se>
2022-10-28 11:03:18 +0200

    stuffy stuff

---
+++ src/_data/country.js
@@ -0,0 +1,12 @@
+const EleventyFetch = require("@11ty/eleventy-fetch");
+require('dotenv').config();
+
+module.exports = async function() {
+    let url = `https://restcountries.com/v3.1/name/benin`;
+
+    /* This returns a promise */
+    return EleventyFetch(url, {
+        duration: "1d", // save for 1 day
+        type: "json"    // we’ll parse JSON for you
+    });
+};
